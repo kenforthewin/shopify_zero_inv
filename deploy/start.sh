2448d1cface224b9b07cb98a37d83c628b577a94
Kenny <kenny@nurelm.com>
2018-01-28 10:01:23 -0500

    fix

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,9 +2,9 @@
 set -e
 bundle install
 bundle exec rake db:create db:migrate
-bundle exec puma -C config/puma.rb -d
 if [ "$RAILS_ENV" = "production" ]; then
   npm install
   rake assets:precompile
 fi
+bundle exec puma -C config/puma.rb -d
 exec nginx -g 'daemon off;'
