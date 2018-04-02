106d88a5f673e4a0dad22d24f69326240b46d3d3
Kenny <kenny@nurelm.com>
2018-04-02 13:10:18 -0400

    prod updates

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.prod.yml -f docker-compose.yml up --build -d
-docker network connect hcr_esnet shopifybucks_app_1
-docker network connect hcr_esnet shopifybucks_sidekiq_1
-docker network connect nginx_network shopifybucks_app_1
+docker network connect hcr_esnet shopifyzeroinv_app_1
+docker network connect hcr_esnet shopifyzeroinv_sidekiq_1
+docker network connect nginx_network shopifyzeroinv_app_1
