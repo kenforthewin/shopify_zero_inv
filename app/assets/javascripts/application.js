ce48b8557927cda38d4ba99f16e5e7335fc38ac9
Kenny <bergquist.kenneth@gmail.com>
2018-04-09 19:11:35 -0400

    cors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,16 +19,6 @@
 //= require datatables/media/js/jquery.dataTables
 //= require_tree .
 const permissionUrl = '/oauth/authorize?client_id=09f29aa8e5c5f71b2a7b6e4026d333b9&scope=read_products,read_orders&redirect_uri=https://shopify.kenforthewin.com/auth/shopify/callback';
-
-// If the current window is the 'parent', change the URL by setting location.href
-if (window.top == window.self) {
-  url = 'https://' + shopDomain + "/admin/" + permissionUrl;
-  window.location.assign(url);
-
-// If the current window is the 'child', change the parent's URL with ShopifyApp.redirect
-} else {
-  ShopifyApp.redirect(permissionUrl);
-}
 $(document).on('turbolinks:load', function() {
   ShopifyApp.Bar.loadingOff();
 
@@ -36,5 +26,13 @@ $(document).on('turbolinks:load', function() {
     ShopifyApp.Bar.loadingOff();
   });
 
+  // If the current window is the 'parent', change the URL by setting location.href
+  if (window.top == window.self) {
+    url = 'https://' + shopDomain + "/admin/" + permissionUrl;
+    window.location.assign(url);
+  // If the current window is the 'child', change the parent's URL with ShopifyApp.redirect
+  } else {
+    ShopifyApp.redirect(permissionUrl);
+  }
 
 })
