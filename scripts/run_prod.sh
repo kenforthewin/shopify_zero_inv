1217bb7efe4f30ea6e0f8bba9bd8ed9658cd0d0c
Kenny <bergquist.kenneth@gmail.com>
2019-01-13 12:44:00 -0500

    prod override

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose -f docker-compose.prod.yml -f docker-compose.yml up --build -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
 docker network connect nginx_network shopifyzeroinv_app_1
